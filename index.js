e08058efcbe0560567bb1c38aedbebd46d2f71b9
Kirill Yaymchuk <stbestich@gmail.com>
2023-12-20 20:27:13 +0200

    Installed cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import cors from 'cors';
 
 const PORT = 3000;
 const HOST = 'localhost';
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
