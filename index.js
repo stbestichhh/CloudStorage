359738320acccd7649cc00887dce47ef77d90605
Kirill Yaymchuk <stbestich@gmail.com>
2023-12-20 20:35:07 +0200

    Create directory

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import cors from 'cors';
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
 const HOST = 'localhost';
 
 const app = express();
 app.use(cors());
+
+const uploadDirectory = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
 
 app.get('/', (req, res) => {
   res.json({
