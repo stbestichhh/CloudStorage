801fb526adf2e620dd5aa561eea25d12639fc64a
Kirill Yaymchuk <stbestich@gmail.com>
2023-12-20 21:52:33 +0200

    Created controllers

---
+++ controllers/fileController.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import uploadDirectoryPath from './directoryController.js';
+import validateFileArray from '../validations/fileArray.js';
+import path from 'path';
+import CheckPath from '../utils/checkPath.js';
+
+export const GetAllFiles = (req, res) => {
+  fs.readdir(uploadDirectoryPath, (err, files) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    res.status(200).json(files);
+  });
+};
+
+export const UploadFile = (req, res) => {
+  if (!req.files || validateFileArray(req.files)) {
+    return res.status(400).json('You have to uplopad a file.');
+  }
+
+  res.json('Files uploaded.');
+};
+
+export const DownloadFile = (req, res) => {
+  const filepath = path.join(uploadDirectoryPath, req.params.filename);
+  CheckPath(filepath, () => res.status(400).json('File not exists.'));
+
+  res.status(200).download(filepath);
+};
+
+export const DeleteFile = (req, res) => {
+  const filepath = path.join(uploadDirectoryPath, req.params.filename);
+  CheckPath(filepath, () => res.status(400).json('File not exists.'));
+
+  fs.unlink(filepath, (err) => {
+    if (err) {
+      return res.status(500).json('Unexprected error.');
+    }
+    res.status(200).json('File deleted.');
+  });
+};
--- index.js
+++ index.js
@@ -2,14 +2,8 @@
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import CheckPath from './utils/checkPath.js';
-import validateFileArray from './validations/fileArray.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import uploadDirectoryPath, { CreateDirectory } from './controllers/directoryController.js';
+import { DeleteFile, DownloadFile, GetAllFiles, UploadFile } from './controllers/fileController.js';
 
 const PORT = 3000;
 const HOST = 'localhost';
@@ -17,10 +11,7 @@ const HOST = 'localhost';
 const app = express();
 app.use(cors());
 
-const uploadDirectoryPath = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDirectoryPath)) {
-  fs.mkdir(uploadDirectoryPath, (err) => console.error(err));
-}
+CreateDirectory();
 
 const storage = multer.diskStorage({
   destination: uploadDirectoryPath,
@@ -37,42 +28,10 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/files', (req, res) => {
-  fs.readdir(uploadDirectoryPath, (err, files) => {
-    if (err) {
-      return res.status(500).json(err);
-    }
-
-    res.status(200).json(files);
-  });
-});
-
-app.post('/upload', upload.array('files'), (req, res) => {
-  if (!req.files || validateFileArray(req.files)) {
-    return res.status(400).json('You have to uplopad a file.');
-  }
-
-  res.json('Files uploaded.');
-});
-
-app.get('/download/:filename', (req, res) => {
-  const filepath = path.join(uploadDirectoryPath, req.params.filename);
-  CheckPath(filepath, () => res.status(400).json('File not exists.'));
-
-  res.status(200).download(filepath);
-});
-
-app.delete('/delete/:filename', (req, res) => {
-  const filepath = path.join(uploadDirectoryPath, req.params.filename);
-  CheckPath(filepath, () => res.status(400).json('File not exists.'));
-
-  fs.unlink(filepath, (err) => {
-    if (err) {
-      return res.status(500).json('Unexprected error.');
-    }
-    res.status(200).json('File deleted.');
-  });
-});
+app.get('/files', GetAllFiles);
+app.post('/upload', upload.array('files'), UploadFile);
+app.get('/download/:filename', DownloadFile);
+app.delete('/delete/:filename', DeleteFile);
 
 app.listen(PORT, () => {
   console.log(`Server running at port: ${PORT} on http://${HOST}:${PORT}`);
